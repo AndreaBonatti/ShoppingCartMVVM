5c09bd1d8ed645eaab3f263e4ac25a1d9419a905
Andrea Bonatti <andreabonatti92@gmail.com>
2021-12-07 12:04:13 +0100

    Product Detail Fragment + Share ViewModel

--- app/src/main/java/com/example/shoppingcartmvvm/viewmodels/ShopViewModel.java
+++ app/src/main/java/com/example/shoppingcartmvvm/viewmodels/ShopViewModel.java
@@ -1,6 +1,7 @@
 package com.example.shoppingcartmvvm.viewmodels;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.shoppingcartmvvm.models.Product;
@@ -12,7 +13,17 @@ public class ShopViewModel extends ViewModel {
 
     ShopRepo shopRepo = new ShopRepo();
 
+    MutableLiveData<Product> mutableProduct = new MutableLiveData<>();
+
     public LiveData<List<Product>> getProducts() {
         return shopRepo.getProducts();
+    }
+
+    public void setProduct(Product product) {
+        mutableProduct.setValue(product);
+    }
+
+    public LiveData<Product> getProduct() {
+        return mutableProduct;
     }
 }
--- app/src/main/java/com/example/shoppingcartmvvm/views/ProductDetailFragment.java
+++ app/src/main/java/com/example/shoppingcartmvvm/views/ProductDetailFragment.java
@@ -2,15 +2,23 @@ package com.example.shoppingcartmvvm.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.shoppingcartmvvm.R;
+import com.example.shoppingcartmvvm.databinding.FragmentProductDetailBinding;
+import com.example.shoppingcartmvvm.viewmodels.ShopViewModel;
 
 public class ProductDetailFragment extends Fragment {
+
+    FragmentProductDetailBinding fragmentProductDetailBinding;
+    ShopViewModel shopViewModel;
 
     public ProductDetailFragment() {
         // Required empty public constructor
@@ -20,6 +28,15 @@ public class ProductDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_detail, container, false);
+        fragmentProductDetailBinding = FragmentProductDetailBinding.inflate(inflater, container, false);
+        return fragmentProductDetailBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        shopViewModel = new ViewModelProvider(requireActivity()).get(ShopViewModel.class);
+        fragmentProductDetailBinding.setShopViewModel(shopViewModel);
     }
 }
