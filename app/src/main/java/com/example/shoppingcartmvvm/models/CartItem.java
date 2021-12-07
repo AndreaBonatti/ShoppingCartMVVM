a0d0ffcdd6878736429b00cec1ec0cd4ea92cc4a
Andrea Bonatti <andreabonatti92@gmail.com>
2021-12-07 14:18:07 +0100

    Cart Model & Layouts

---
+++ app/src/main/java/com/example/shoppingcartmvvm/models/CartItem.java
@@ -0,0 +1,39 @@
+package com.example.shoppingcartmvvm.models;
+
+import java.util.Objects;
+
+public class CartItem {
+
+    private Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "product=" + product +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartItem cartItem = (CartItem) o;
+        return getQuantity() == cartItem.getQuantity() && getProduct().equals(cartItem.getProduct());
+    }
+
+}
